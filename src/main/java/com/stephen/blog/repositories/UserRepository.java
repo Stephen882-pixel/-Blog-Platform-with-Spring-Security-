99d887a15185b2cc0671fa8622326c1482cc73dd
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-06-12 23:50:45 +0300

    loading users

--- src/main/java/com/stephen/blog/repositories/UserRepository.java
+++ src/main/java/com/stephen/blog/repositories/UserRepository.java
@@ -3,10 +3,12 @@ package com.stephen.blog.repositories;
 import com.stephen.blog.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    Optional<User> findByEmail(String email);
 }
